c37b941b7c359ce585ae24c733c5a7bea4d94f11
LynAudubon <lyn_audubon07@yahoo.com>
2021-10-11 18:11:40 -0700

    added new Postgress URL

--- client/components/Banner.jsx
+++ client/components/Banner.jsx
@@ -3,14 +3,17 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 function Banner() {
   return (
-    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-            Medical Practice Inventory Tracker
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
+    <AppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          Medical Practice Inventory Tracker
+        </Typography>
+        <Button color="inherit">Logout</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
--- server/models/inventoryModel.js
+++ server/models/inventoryModel.js
@@ -1,16 +1,19 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+
+const olddatabaseLink =
+  'postgres://sowqmvmc:C8VcOF5uXm7v7Utsy-wL7WEor3YPR-Tq@fanny.db.elephantsql.com/sowqmvmc';
 
 const PG_URI =
-  "postgres://sowqmvmc:C8VcOF5uXm7v7Utsy-wL7WEor3YPR-Tq@fanny.db.elephantsql.com/sowqmvmc"
+  'postgres://voqjozka:mCMKB9lQ-LIJ-gniDzNBroZonytfDEuv@fanny.db.elephantsql.com/voqjozka';
 
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString: PG_URI,
-})
+  connectionString: PG_URI
+});
 
 module.exports = {
   query: (text, params, callback) => {
-    console.log("executed query", text)
-    return pool.query(text, params, callback)
-  },
-}
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  }
+};
